730774ede328b342c43769d06bdb6213ddad3d1d
abdulhasanallahverdiyev <abdulhsn.allahverdiyev@mail.ru>
2023-07-01 07:26:50 +0400

    change

--- backend/FortuneMediaBackEnd/settings.py
+++ backend/FortuneMediaBackEnd/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-al)3emz@@%ihqt3d8*7rgi_8(60q-&k_1d%#49_k$(h^adqtes'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['164.90.163.43']
 
 
 # Application definition
--- frontend/src/httpRequest/httpRequest.js
+++ frontend/src/httpRequest/httpRequest.js
@@ -1,1 +1,1 @@
-export const BASE_URL="http://localhost:8000/"
+export const BASE_URL="http://164.90.163.43/"
